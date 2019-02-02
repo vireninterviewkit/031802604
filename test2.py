0cd722d37a0975bd416b5c4ea30c96b4706b70f0
AD <1424196139@qq.com>
2019-02-02 09:48:58 +0800

    add 3 files.

---
+++ test2.py
@@ -0,0 +1,21 @@
+f=open("Request.txt","r")
+
+res=dict()
+out=[]
+
+for a in f :
+    b,c=a.split(" Bob ")
+    if(b in out):
+        continue #在实际中应该一探测到有问题就停止该用户的访问（这里也确实如此），但在这里先假定无停止用户访问，所以用这个判定做一次拦截。防止出现一人出现多次。
+    if(b in res):
+        res[b]+=int(c)
+    else:
+        res[b]=int(c)
+    if(res[b]>1500):
+        out.append(b)
+
+f.close()
+f = open("output.txt","w")
+for i in out:
+    f.write(i+"\n")
+f.close()
